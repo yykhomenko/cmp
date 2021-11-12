69aec5e65cbb913af3fa5cd1dd6caeba5744eaf7
yykhomenko <yykhomenko@gmail.com>
2021-11-12 10:22:12 +0200

    added panicHandler

--- main.go
+++ main.go
@@ -23,3 +23,18 @@ func main() {
 	fmt.Println("starting server at :8080")
 	http.ListenAndServe(":8080", siteHandler)
 }
+
+func panicHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("panic middleware", r.URL.Path)
+
+		defer func() {
+			if err := recover(); err != nil {
+				fmt.Println("recovered", err)
+				http.Error(w, "Internal server error", 500)
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
