1560b73ed12b6afdc1a37f9bb1252c7dd7f08cf5
yykhomenko <yykhomenko@gmail.com>
2021-11-12 09:53:48 +0200

    added base code for adminHatdler

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import "net/http"
+
 func main() {
+	adminMux := http.NewServeMux()
+	adminMux.HandleFunc("/admin/", adminIndex)
+	adminMux.HandleFunc("/admin/panic/", panicPage)
+	adminHandler := adminAuthMiddleware(adminMux)
 
 }
