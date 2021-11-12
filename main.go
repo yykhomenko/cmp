7dcf437a321f5fe3a05651c332797272f933c8ab
yykhomenko <yykhomenko@gmail.com>
2021-11-12 20:55:30 +0200

    added login link

--- main.go
+++ main.go
@@ -12,8 +12,20 @@ func main() {
 	r := gin.Default()
 	store := cookie.NewStore([]byte("secret"))
 	r.Use(sessions.Sessions("mysession", store))
+
+	r.GET("/", mainPage)
 	r.GET("/login", loginPage)
 	r.Run()
+}
+
+func mainPage(c *gin.Context) {
+	session := sessions.Default(c)
+	id := session.Get("session_id")
+	if id == nil {
+		c.String(http.StatusOK, "You need to login %s", `<a href="/login">login</a>`)
+	} else {
+		c.String(http.StatusOK, "Hello, %s", id)
+	}
 }
 
 func loginPage(c *gin.Context) {
