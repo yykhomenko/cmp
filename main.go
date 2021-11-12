e39620a52024b0655533a53d43641f17a9ba207f
yykhomenko <yykhomenko@gmail.com>
2021-11-12 20:37:38 +0200

    init with login page on GIN

--- main.go
+++ main.go
@@ -1,115 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"time"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	adminMux := http.NewServeMux()
-	adminMux.HandleFunc("/admin/", adminIndex)
-	adminMux.HandleFunc("/admin/panic/", panicPage)
-	adminHandler := adminAuthMiddleware(adminMux)
-
-	siteMux := http.NewServeMux()
-	siteMux.Handle("/admin/", adminHandler)
-	siteMux.HandleFunc("/login", loginPage)
-	siteMux.HandleFunc("/logout", logoutPage)
-	siteMux.HandleFunc("/", mainPage)
-
-	siteHandler := accessLogMiddleware(siteMux)
-	siteHandler = panicMiddleware(siteHandler)
-
-	fmt.Println("starting server at :8080")
-	http.ListenAndServe(":8080", siteHandler)
+	r := gin.Default()
+	store := cookie.NewStore([]byte("secret"))
+	r.Use(sessions.Sessions("mysession", store))
+	r.GET("/login", loginPage)
+	r.Run()
 }
 
-func panicMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("panic middleware", r.URL.Path)
-
-		defer func() {
-			if err := recover(); err != nil {
-				fmt.Println("recovered", err)
-				http.Error(w, "Internal server error", 500)
-			}
-		}()
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-func accessLogMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("accessLogMiddleware", r.URL.Path)
-		start := time.Now()
-		next.ServeHTTP(w, r)
-		fmt.Printf("[%s] %s, %s %s\n",
-			r.Method, r.RemoteAddr, r.URL.Path, time.Since(start))
-	})
-}
-
-func adminAuthMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("adminAuthMiddleware", r.URL.Path)
-
-		//! auth dummy
-		_, err := r.Cookie("session_id")
-		if err != nil {
-			fmt.Println("no auth at", r.URL.Path)
-			http.Redirect(w, r, "/", http.StatusFound)
-			return
-		}
-		//! end of auth dummy
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-func panicPage(w http.ResponseWriter, r *http.Request) {
-	panic("this must be recovered")
-}
-
-func adminIndex(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, `<a href="/">site index</a>`)
-	fmt.Fprintln(w, "Admin main page")
-}
-
-func loginPage(w http.ResponseWriter, r *http.Request) {
-	expiration := time.Now().Add(10 * time.Hour)
-	cookie := http.Cookie{
-		Name:    "session_id",
-		Value:   "yykhomenko",
-		Expires: expiration,
-	}
-	http.SetCookie(w, &cookie)
-	http.Redirect(w, r, "/", http.StatusFound)
-}
-
-func logoutPage(w http.ResponseWriter, r *http.Request) {
-	session, err := r.Cookie("session_id")
-	if err == http.ErrNoCookie {
-		http.Redirect(w, r, "/", http.StatusFound)
-		return
-	}
-	session.Expires = time.Now().AddDate(0, 0, -1)
-	http.SetCookie(w, session)
-	http.Redirect(w, r, "/", http.StatusFound)
-}
-
-func mainPage(w http.ResponseWriter, r *http.Request) {
-	session, err := r.Cookie("session_id")
-
-	//! auth dummy
-	loggedIn := (err != http.ErrNoCookie)
-	//! end of auth dummy
-
-	if loggedIn {
-		fmt.Fprintln(w, `<a href="/logout">logout</a>`)
-		fmt.Fprintln(w, "Welcome, "+session.Value)
-	} else {
-		fmt.Fprintln(w, `<a href="/login">login</a>`)
-		fmt.Fprintln(w, "You need to login")
-	}
+func loginPage(c *gin.Context) {
+	session := sessions.Default(c)
+	session.Set("session_id", "yykhomenko")
+	session.Save()
+	c.Redirect(http.StatusFound, "/")
 }
