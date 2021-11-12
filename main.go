3433235a42b76d03deebf3a50cd349cb86cc0bb8
yykhomenko <yykhomenko@gmail.com>
2021-11-12 11:09:54 +0200

    added logoutPage

--- main.go
+++ main.go
@@ -86,3 +86,14 @@ func loginPage(w http.ResponseWriter, r *http.Request) {
 	http.SetCookie(w, &cookie)
 	http.Redirect(w, r, "/", http.StatusFound)
 }
+
+func logoutPage(w http.ResponseWriter, r *http.Request) {
+	session, err := r.Cookie("sessiod_id")
+	if err == http.ErrNoCookie {
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+	session.Expires = time.Now().AddDate(0, 0, -1)
+	http.SetCookie(w, session)
+	http.Redirect(w, r, "/", http.StatusFound)
+}
