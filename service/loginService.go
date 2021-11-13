9b545567dd5dfb85029afcce21d133f54c390759
yykhomenko <yykhomenko@gmail.com>
2021-11-13 11:54:03 +0200

    rename

---
+++ service/loginService.go
@@ -0,0 +1,22 @@
+package service
+
+import "github.com/yykhomenko/cmp/repository"
+
+type LoginService interface {
+	LoginUser(login string, password string) bool
+}
+
+type loginService struct {
+	userRepo repository.UserRepository
+}
+
+func NewLoginService(userRepo repository.UserRepository) LoginService {
+	return &loginService{
+		userRepo: userRepo,
+	}
+}
+
+func (ls *loginService) LoginUser(login string, password string) bool {
+	user := ls.userRepo.Get(login)
+	return user.Login == login && user.Password == password
+}
