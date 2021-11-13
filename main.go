f7d558f7bd37e2b37edbe3ae9d88292b119faac9
yykhomenko <yykhomenko@gmail.com>
2021-11-13 09:41:02 +0200

    clean

--- main.go
+++ main.go
@@ -1,18 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
-	"github.com/gin-contrib/sessions"
-	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	store := cookie.NewStore([]byte("secret"))
-	r.Use(sessions.Sessions("mysession", store))
 
 	r.GET("/", mainPage)
 	r.GET("/login", loginPage)
@@ -21,27 +16,13 @@ func main() {
 }
 
 func mainPage(c *gin.Context) {
-	session := sessions.Default(c)
-	id := session.Get("session_id")
-	if id == nil {
-		c.Data(http.StatusOK, "text/html; charset=utf-8",
-			[]byte(`You need to login <a href="/login">login</a>`))
-	} else {
-		c.Data(http.StatusOK, "text/html; charset=utf-8",
-			[]byte(fmt.Sprintf("Hello, %s %s", id, `<a href="/logout">logout</a>`)))
-	}
+
 }
 
 func loginPage(c *gin.Context) {
-	session := sessions.Default(c)
-	session.Set("session_id", "yykhomenko")
-	session.Save()
 	c.Redirect(http.StatusFound, "/")
 }
 
 func logoutPage(c *gin.Context) {
-	session := sessions.Default(c)
-	session.Clear()
-	session.Save()
 	c.Redirect(http.StatusFound, "/")
 }
