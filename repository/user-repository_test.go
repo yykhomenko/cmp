133657eaf999b0c36edfb199b2a164bb08a08bfb
yykhomenko <yykhomenko@gmail.com>
2021-11-13 11:19:34 +0200

    write set get test user repo

--- repository/user-repository_test.go
+++ repository/user-repository_test.go
@@ -4,11 +4,19 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/yykhomenko/cmp/entity"
 )
 
-func TestUser(t *testing.T) {
-	login := "yykhomenko"
-	repo := NewUserRepository()
-	user := repo.Get(login)
-	assert.Equal(t, login, user.Login)
+func TestSetGet(t *testing.T) {
+	testUser := entity.User{
+		Login:    "yykhomenko",
+		Password: "123",
+	}
+
+	userRepository := NewUserRepository()
+	userRepository.Set(testUser)
+	actualUser := userRepository.Get(testUser.Login)
+
+	assert.Equal(t, testUser.Login, actualUser.Login)
+	assert.Equal(t, testUser.Password, actualUser.Password)
 }
