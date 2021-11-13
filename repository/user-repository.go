283129438e863a41de22e920bf2f7b349e4cc5e8
yykhomenko <yykhomenko@gmail.com>
2021-11-13 10:34:57 +0200

    rename

---
+++ repository/user-repository.go
@@ -0,0 +1,27 @@
+package repository
+
+import "github.com/yykhomenko/cmp/entity"
+
+type UserRepository interface {
+	Get(login string) entity.User
+}
+
+type database struct {
+	data map[string]entity.User
+}
+
+func NewUserRepository() UserRepository {
+	testUser := entity.User{
+		Login:    "yykhomenko",
+		Password: "123",
+	}
+	data := make(map[string]entity.User)
+	data[testUser.Login] = testUser
+	return &database{
+		data: data,
+	}
+}
+
+func (db *database) Get(login string) entity.User {
+	return db.data[login]
+}
