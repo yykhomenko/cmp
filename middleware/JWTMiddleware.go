0305499f96aff656048a8c2f33d55a1a94ab0083
yykhomenko <yykhomenko@gmail.com>
2021-11-17 10:15:51 +0200

    added jwt middleware

---
+++ middleware/JWTMiddleware.go
@@ -0,0 +1,26 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"github.com/yykhomenko/cmp/service"
+)
+
+func AutorizeJWT() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		const BEARER_SCHEMA = "Bearer"
+		authHeader := c.GetHeader("Authorization")
+		tokenString := authHeader[len(BEARER_SCHEMA):]
+		token, err := service.NewJWTService().ValidateToken(tokenString)
+		if token.Valid {
+			claims := token.Claims.(jwt.MapClaims)
+			fmt.Println(claims)
+		} else {
+			fmt.Println(err)
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+	}
+}
